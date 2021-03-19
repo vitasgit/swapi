0695e11953fd5f942d846d274b4cdf3ee2d76c1f
vitasgit <vitasanon@mail.ru>
2021-03-19 18:53:58 +0500

    add preview

--- js/main.js
+++ js/main.js
@@ -15,13 +15,49 @@ let editPopup = document.querySelectorAll('.edit-popup');
 let editPopupSwitcher = false;
 
 $('#edit').on('click', function() {
-	editPopupSwitcher = !editPopupSwitcher;
 	this.classList.remove('pulse');
+	editPopupSwitcher = !editPopupSwitcher;
 
 	if (editPopupSwitcher){
-      TweenMax.to(editPopup,1,{x:'2%',display:'block',height:'65vh',ease:Back.easeOut});
-      } 
-  	else {
-        TweenMax.to(editPopup,1,{x:'-200%',display:'block',height:'0vh',ease:Power3.easeOut});
+  		TweenMax.to(editPopup,1,{x:'2%',display:'block',height:'65vh',ease:Back.easeOut});
+  		editPopupSwitcher = !editPopupSwitcher;
       }
+});
+
+$('.content-wrapper').on('click', function(event) {
+	TweenMax.to(editPopup,1,{x:'-200%',display:'block',height:'0vh',ease:Power3.easeOut});
+});
+
+//работа с превью
+let preview = $('.preview-wrapper');
+let themeSelects = document.querySelectorAll('.theme-select');
+
+let prewiewChild = function(){
+    let nodes = [];
+
+    let progressLine = $('.progress-line');
+
+    nodes.push(progressLine[progressLine.length-1]);
+    nodes.push($('.preview-wrapper>a')[0]);
+    return nodes;
+};
+
+let themePreview = {
+    themeType:'indigo-theme',
+    secondaryColor:'#4ebdd4',
+};
+
+$('.theme-select').on('click',function () {
+    for (let el of  themeSelects){
+        el.classList.remove('selected-theme');
+    }
+    this.classList.add('selected-theme');
+
+    if (this.id === 'indigo'){
+        preview[0].classList.value = 'preview-wrapper indigo-theme';
+        themePreview.themeType = 'indigo-theme';
+    } else {
+        preview[0].classList.value = 'preview-wrapper black-theme';
+        themePreview.themeType = 'black-theme';
+    }
 });
